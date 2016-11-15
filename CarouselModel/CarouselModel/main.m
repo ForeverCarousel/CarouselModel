1594ebaaeec6a9b70b13eada613b64b655d0bc02
Chenxiaolong <chenxiaolong@youku.com>
2016-11-15 20:35:37 +0800

    project init

---
+++ CarouselModel/CarouselModel/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CarouselModel
+//
+//  Created by Carouesl on 2016/11/15.
+//  Copyright © 2016年 Carouesl. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool
+    {
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
